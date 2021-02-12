d99a4fd2be44cd10ce97cdc793c94f512d579adb
Ekateryna Gruvman <ekaterynagruvman@Ekaterynas-MacBook-Air.local>
2021-02-12 20:57:28 +0200

    commit

---
+++ src/JS/templating.js
@@ -0,0 +1,7 @@
+import galleryItem from './../templates/menu.hbs';
+import menu from '../menu.json';
+
+const  markUp = galleryItem(menu);
+const galleryRef = document.querySelector('.js-menu');
+
+galleryRef.insertAdjacentHTML('beforeend', markUp);
